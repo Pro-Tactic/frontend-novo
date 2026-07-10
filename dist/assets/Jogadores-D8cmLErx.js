6d159997015fbf2c9c5556b0ec583dc384069b2c
lucas sukar <lfsw@cesar.school>
2026-07-10 18:30:33 -0300

    taticas

--- dist/assets/Jogadores-D8cmLErx.js
+++ dist/assets/Jogadores-D8cmLErx.js
@@ -1,4 +1,4 @@
-import{c as x,r as a,g as m,j as e,A as u,a as f}from"./index-jtC_hgqk.js";import{S as h}from"./search-DaeD2hXm.js";/**
+import{c as x,r as a,g as m,j as e,A as u,a as f}from"./index-BtKPmj-W.js";import{S as h}from"./search-CgUUCxn1.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/Registro-BlC7-ymA.js
+++ dist/assets/Registro-BlC7-ymA.js
@@ -1,4 +1,4 @@
-import{c as p,u as x,r as l,b as u,d as b,j as e,l as m,T as n,L as h,S as g,e as f,w as j,f as w}from"./index-jtC_hgqk.js";import{P as a}from"./plus-B5CD3pMI.js";import{S as N}from"./search-DaeD2hXm.js";/**
+import{c as p,u as x,r as l,b as u,d as b,j as e,l as m,T as n,L as h,S as g,e as f,w as j,f as w}from"./index-BtKPmj-W.js";import{P as a}from"./plus-BfF16vdE.js";import{S as N}from"./search-CgUUCxn1.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
